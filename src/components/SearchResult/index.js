b0ab9ae8d3e1de725bdb121bf8e837ce53f065b8
Emmanuel Chilaka <emmanuelchilaka779@gmail.com>
2022-07-27 21:12:41 +0300

    fix issues in search result

--- src/components/SearchResult/index.js
+++ src/components/SearchResult/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./result.css";
 
 export default function Users({ users }) {
-//   console.log(users);
+  console.log(users);
   return (
     <div className="users-list">
       <div className="container">
@@ -10,8 +10,24 @@ export default function Users({ users }) {
           <div className="box" key={index}>
             <div className="box-content">
               <img src={user.avatar_url} alt={user.login} />
-              <div className="details">
-                <h3>{user.login} <a href={user.html_url}><span>@{user.login}</span></a></h3>
+
+              <div className="box-flex">
+                <h1 className="details">
+                  {user.login.substring(0, 5) + '...'}{" "}
+                  <a href={user.html_url}>
+                    <span>@{user.login}</span>
+                  </a>
+                </h1>
+                <div className="followers">
+                  <div>
+                    <h2>Followers</h2>
+                    <p>{user.followers_url.length}</p>
+                  </div>
+                  <div>
+                    <h2>Following</h2>
+                    <p>{user.following_url.length}</p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
